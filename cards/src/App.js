f4619754b3767482468d7147c483cd0a6ca0c940
Avi111111 <72097855+Avi111111@users.noreply.github.com>
2021-08-24 13:00:23 +0300

    add column

--- cards/src/App.js
+++ cards/src/App.js
@@ -54,12 +54,12 @@ function App() {
                                                 width={280}
                                                 truncatedEndingComponent={"... "}  className='grid-item'>{data.resource}</ShowMoreText>
                                                 <div className='grid-item'> {data.category}</div>
-                                                <div className='grid-item' > {parseTime(Number(data.creation_date))}   
-                                                </div>
+                                                <div className='grid-item' > {parseTime(Number(data.creation_date))}</div>
+                                                <div className='grid-item'> {data.errors}</div>
                                     
                           </div>
-                           {isActive===row_index &&  <div className="content"><p>Status : {data.details.status} </p> 
-                          <p>Severity :{data.details.severity}</p> <p> Tags :{JSON.stringify(data.details.tags).replace(/[^a-zA-Z0-9 : ,_ ]/g, " ") }</p></div>}
+                           {isActive===row_index &&  <div className="content"><p><b>Status</b> : {data.details.status} </p> 
+                          <p><b>Severity </b>:{data.details.severity}</p> <p> <b>Tags</b> :{JSON.stringify(data.details.tags).replace(/[^a-zA-Z0-9 : ,_ ]/g, " ") }</p></div>}
                         </div>
       
       )
